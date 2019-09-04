415d440b491e1f920f8786881bedc3f184b1f420
SirBubbls <lucassas@live.de>
2019-09-04 13:28:06 +0200

    Authentication

--- EasyReddit/Authentication/AuthenticationManager.swift
+++ EasyReddit/Authentication/AuthenticationManager.swift
@@ -1,9 +1,119 @@
 //
 //  AuthenticationManager.swift
-//  EasyReddit
+//  reddit
 //
-//  Created by Lucas Sas on 04.09.19.
+//  Created by Lucas Sas on 08.08.19.
 //  Copyright © 2019 Lucas Sas. All rights reserved.
 //
 
 import Foundation
+import Cocoa
+
+public class AuthenticationManager {
+    let clientId: String
+    var token: OAuthToken?
+    var state = "irasntuwfynta" // Needs to be randomly generated
+    let redirect_uri = "redditapp://response"
+    let loginWindow: NSViewController
+    
+    
+    static let shared = AuthenticationManager(clientId: "i6wWXtGXhwuZoA")
+    
+    init(clientId: String) {
+        self.clientId = clientId
+        
+        
+        let storyboard: NSStoryboard = NSStoryboard(name: "Main", bundle: nil)
+        self.loginWindow = storyboard.instantiateController(withIdentifier: "login") as! NSViewController
+        
+        // Try to Load Token
+        do{
+            self.token = try self.loadToken()
+            
+            print("Token Loaded")
+        }catch{
+            print("Token Could not be Loaded")
+        }
+        
+        refresh()
+        
+        
+    }
+    
+    
+    func checkToken() -> Bool {
+        
+        
+        return true
+    }
+    
+    
+    
+    func loadToken() throws -> OAuthToken? {
+        print("Trying To Load Token")
+        let defaults:UserDefaults = UserDefaults.standard
+        
+        let accessToken = defaults.string(forKey: "accessToken")
+        let refreshToken = defaults.string(forKey: "refreshToken")
+        
+        if accessToken == nil && refreshToken == nil{
+            return nil
+        }
+        
+        return OAuthToken(accessToken: accessToken!, refreshToken: refreshToken!)
+        
+    }
+    
+    
+    func requestNewToken() {
+        let authUrl = "https://www.reddit.com/api/v1/authorize?client_id=\(self.clientId)&response_type=code&state=\(self.state)&redirect_uri=\(self.redirect_uri)&duration=permanent&scope=identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,submit,subscribe,vote,wikiedit,wikiread"
+        
+        NSWorkspace.shared.open(URL(string: authUrl)!)
+    }
+    
+    func viableToken() -> Bool {
+        
+        return false
+    }
+    
+    func refresh() {
+        let postData = "grant_type=refresh_token&refresh_token=\(token!.refreshToken)"
+        let credentials = String(format: "%@:", self.clientId)
+        print("ClientID", self.clientId)
+        let request = PostRequest(url: URL(string: "https://www.reddit.com/api/v1/access_token")!, credentials: credentials, postData: postData)
+        let json = request.makeRequest()
+        
+        let token: String = json["access_token"].stringValue
+        
+        if token > "" {
+            self.token?.accessToken = token
+            self.token?.save()
+        }
+        
+    }
+    
+    func getTokenByCode(code: String) {
+        let postData = "grant_type=authorization_code&code=\(code)&redirect_uri=\(self.redirect_uri)"
+        let credentials = String(format: "%@:", self.clientId)
+        print("ClientID", self.clientId)
+        let request = PostRequest(url: URL(string: "https://www.reddit.com/api/v1/access_token")!, credentials: credentials, postData: postData)
+        let json = request.makeRequest()
+        self.token = OAuthToken(response: json)
+        self.token?.save()
+    }
+    
+    
+    func interceptCode(url: URL) {
+        print(url)
+        let information = String(url.query!)
+        
+        let arguments = information.split(separator: "&")
+        //
+        let code = String(arguments[1].split(separator: "=")[1])
+        self.state = String(arguments[0].split(separator: "=")[1])
+        
+        print("Code Intercepted: \(code)")
+        
+        self.getTokenByCode(code: String(code))
+    }
+}
--- EasyReddit/Authentication/OAuthToken.swift
+++ EasyReddit/Authentication/OAuthToken.swift
@@ -1,9 +1,56 @@
 //
 //  OAuthToken.swift
-//  EasyReddit
+//  reddit
 //
-//  Created by Lucas Sas on 04.09.19.
+//  Created by Lucas Sas on 08.08.19.
 //  Copyright © 2019 Lucas Sas. All rights reserved.
 //
 
 import Foundation
+
+public class OAuthToken {
+    let scope: String?
+    let refreshToken: String
+    let expiresIn: Int?
+    var accessToken: String
+    let tokenType: String?
+    
+    
+    init(response: JSON) {
+        self.scope = response["scope"].string
+        self.refreshToken = response["refresh_token"].string!
+        self.accessToken = response["access_token"].string!
+        self.expiresIn = response["expires_in"].int
+        self.tokenType = response["token_type"].string
+        
+        self.getInfo()
+    }
+    
+    init(accessToken: String, refreshToken: String) {
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
+        
+        self.scope = nil
+        self.expiresIn = nil
+        self.tokenType = nil
+    }
+    
+    public func getInfo() {
+        print("Scope: \(self.scope ?? "unknown")")
+        print("Refresh Token: \(self.refreshToken)")
+        print("Access Token: \(self.accessToken)")
+        print("Expires In: \(self.expiresIn ?? -1)")
+        print("Token Type: \(self.tokenType ?? "unknown")")
+    }
+    
+    
+    func save(){
+        print("Saving OAuth Token in UserDefaults.standard")
+        let defaults:UserDefaults = UserDefaults.standard
+        
+        defaults.set(self.accessToken, forKey: "accessToken")
+        defaults.set(self.refreshToken, forKey: "refreshToken")
+    }
+    
+    
+}
