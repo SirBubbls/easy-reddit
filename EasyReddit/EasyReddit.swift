395f0540c1a4e92e5e3270b4504530c041876e8a
SirBubbls <lucassas@live.de>
2019-09-04 13:01:09 +0200

    initial

---
+++ EasyReddit/EasyReddit.swift
@@ -0,0 +1,16 @@
+//
+//  EasyReddit.swift
+//  EasyReddit
+//
+//  Created by Lucas Sas on 04.09.19.
+//  Copyright © 2019 Lucas Sas. All rights reserved.
+//
+
+import Foundation
+
+
+public class EasyReddit {
+    public init () {
+        
+    }
+}
---
+++ EasyRedditTests/EasyRedditTests.swift
@@ -0,0 +1,34 @@
+//
+//  EasyRedditTests.swift
+//  EasyRedditTests
+//
+//  Created by Lucas Sas on 04.09.19.
+//  Copyright © 2019 Lucas Sas. All rights reserved.
+//
+
+import XCTest
+@testable import EasyReddit
+
+class EasyRedditTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
