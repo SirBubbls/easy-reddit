aae1f35f175e8171ef934a3317fc02ac00058db7
SirBubbls <lucassas@live.de>
2019-09-04 14:41:05 +0200

    minor

--- EasyReddit/API/Parser.swift
+++ EasyReddit/API/Parser.swift
@@ -1,9 +1,47 @@
 //
 //  Parser.swift
-//  EasyReddit
+//  reddit
 //
-//  Created by Lucas Sas on 04.09.19.
+//  Created by Lucas Sas on 12.08.19.
 //  Copyright © 2019 Lucas Sas. All rights reserved.
 //
 
 import Foundation
+
+public class Parser {
+    
+    static func parseRequest(response: JSON) -> [DataType] {
+        let data = response["data"]["children"].array ?? [JSON]()
+        
+        return Parser.parse(data: data)
+    }
+    
+    static func parse(data: [JSON]) -> [DataType] {
+        var result = [DataType]()
+        
+        for point in data {
+            
+            switch point["kind"].stringValue {
+            case RedditComment.kind:
+                break
+            case RedditAccount.kind:
+                break
+            case RedditLink.kind:
+                let parsedData: RedditLink = RedditLink(json: point["data"])
+                result.append(parsedData)
+            case RedditMessage.kind:
+                break
+            case Subreddit.kind:
+                let parsedData: Subreddit = Subreddit(json: point["data"])
+                result.append(parsedData)
+            case RedditAward.kind:
+                break
+            default:
+                print("Unknown DataType '\(point["kind"].stringValue)'  Error")
+                continue
+            }
+        }
+        
+        return result
+    }
+}
